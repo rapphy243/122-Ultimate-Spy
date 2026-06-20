5896dea6db075afba11467c0677857afc31f2f57
rapphy243 <36800640+rapphy243@users.noreply.github.com>
2026-06-20 02:04:11 -0500

    i still dont like how this is set up

--- grid.h
+++ grid.h
@@ -48,69 +48,70 @@ class Map1 : public Grid {
         Spy* spy;
         std::vector<Guard*> guard;
         bool isValidMove(char direction) {
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
-                    if (grid[i][j]->getIcon() == '@') {
-                        switch (direction) {
-                            case 'w':
-                                return grid[i-1][j]->getIcon() != '#';
-                            case 'a':
-                                return grid[i][j - 1]->getIcon() != '#';
-                            case 's':
-                                return grid[i + 1][j]->getIcon() != '#';
-                            case 'd':
-                                return grid[i][j+1]->getIcon() != '#';
-                        }
-                    }
-                }
+            switch (direction) {
+                case 'w':
+                    return grid[spy->getX() - 1][spy->getY()] == nullptr || grid[spy->getX() - 1][spy->getY()]->getIcon() != '#';
+                case 'a':
+                    return grid[spy->getX()][spy->getY() - 1] == nullptr || grid[spy->getX()][spy->getY() - 1]->getIcon() != '#';
+                case 's':
+                    return grid[spy->getX() + 1][spy->getY()] == nullptr || grid[spy->getX() + 1][spy->getY()]->getIcon() != '#';
+                case 'd':
+                    return grid[spy->getX()][spy->getY() + 1] == nullptr || grid[spy->getX()][spy->getY() + 1]->getIcon() != '#';
+                default:
+                    return false;
             }
         }
     public:
         Map1() {
+            spy = new Spy(5, 1);
             // Walls
-            for (int i = 0; i < 10; i++) {
-                addSprite(0, i, new Wall());
-                addSprite(9, i, new Wall());
-                addSprite(i, 0, new Wall());
-                addSprite(i, 9, new Wall());
+            for (int i = 0; i < 10; i++)
+            {
+                addSprite(0, i, new Wall(0, i));
+                addSprite(9, i, new Wall(9, i));
+                addSprite(i, 0, new Wall(i, 0));
+                addSprite(i, 9, new Wall(i, 9));
             }
-            addSprite(2, 3, new Wall());
-            addSprite(3, 3, new Wall());
-            addSprite(4, 3, new Wall());
-            addSprite(5, 3, new Wall());
-            addSprite(6, 3, new Wall());
+            addSprite(2, 3, new Wall(2, 3));
+            addSprite(3, 3, new Wall(3, 3));
+            addSprite(4, 3, new Wall(4, 3));
+            addSprite(5, 3, new Wall(5, 3));
+            addSprite(6, 3, new Wall(6, 3));
 
             // Goal
-            addSprite(8, 8, new Goal());
+            addSprite(8, 8, new Goal(8, 8));
 
             // Guard
-            addSprite(1, 5, new Guard());
+            addSprite(1, 5, new Guard(1, 5));
 
             // Spy
-            addSprite(5, 1, new Spy());
+            addSprite(spy->getX(), spy->getY(), spy);
         }
         void moveSpy(char direction) {
             if (isValidMove(direction)) {
-                for (int i = 0; i < 10; i++) {
-                    for (int j = 0; j < 10; j++) {
-                        if (grid[i][j]->getIcon() == '@') {
-                            switch (direction) {
-                                case 'w':
-                                    std::swap(grid[i][j], grid[i-1][j]);
-                                    break;
-                                case 'a':
-                                    std::swap(grid[i][j], grid[i][j - 1]);
-                                    break;
-                                case 's':
-                                    std::swap(grid[i][j], grid[i + 1][j]);
-                                    break;
-                                case 'd':
-                                    std::swap(grid[i][j], grid[i][j+1]);
-                                    break;
-                            }
-                        }
-                    }
+                // move spy
+                grid[spy->getX()][spy->getY()] = nullptr;
+                switch (direction) {
+                    case 'w':
+                        spy->setX(spy->getX() - 1);
+                        break;
+                    case 'a':
+                        spy->setY(spy->getY() - 1);
+                        break;
+                    case 's':
+                        spy->setX(spy->getX() + 1);
+                        break;
+                    case 'd':
+                        spy->setY(spy->getY() + 1);
+                        break;
                 }
+                // check if spy is on goal
+                if (grid[spy->getX()][spy->getY()] != nullptr && grid[spy->getX()][spy->getY()]->getIcon() == '$') {
+                    std::cout << "You win!" << std::endl;
+                    exit(0);
+                }
+                if
+                grid[spy->getX()][spy->getY()] = spy;
             }
         }
 
--- sprites.h
+++ sprites.h
@@ -7,32 +7,56 @@
 class Sprite {
     protected:
         char icon;
+        int x;
+        int y;
 
     public:
-        Sprite();
-        Sprite(char i) {
-        icon = i;
-    };
-    char getIcon() {
-        return icon;
-    }
-    friend std::ostream &operator<<(std::ostream &out, const Sprite &s){
-        out << s.icon;
-        return out;
-    }
+        Sprite() {
+            icon = ' ';
+            x = 0;
+            y = 0;
+        }
+        Sprite(char i, int xPos, int yPos) {
+            x = xPos;
+            y = yPos;
+            icon = i;
+        }
+        char getIcon() {
+            return icon;
+        }
+        int getX() {
+            return x;
+        }
+        int getY() {
+            return y;
+        }
+        int setX(int newX) {
+            x = newX;
+        }
+        int setY(int newY) {
+            y = newY;
+        }
+        friend std::ostream &operator<<(std::ostream &out, const Sprite &s) {
+            out << s.icon;
+            return out;
+        }
 };
 
 class Wall : public Sprite {
     public:
-        Wall() {
+        Wall(int xPos, int yPos) {
             icon = '#';
+            x = xPos;
+            y = yPos;
         }
 };
 
 class Goal : public Sprite {
     public:
-        Goal() {
+        Goal(int xPos, int yPos) {
             icon = '$';
+            x = xPos;
+            y = yPos;
         }
 };
 
@@ -52,15 +76,19 @@ class Guard : public Sprite {
     }
 
     public:
-        Guard() {
+        Guard(int xPos, int yPos) {
             icon = randDirection();
+            x = xPos;
+            y = yPos;
         }
 };
 
 class Spy : public Sprite {
     public:
-        Spy() {
+        Spy(int xPos, int yPos) {
             icon = '@';
+            x = xPos;
+            y = yPos;
         }
 };
 
